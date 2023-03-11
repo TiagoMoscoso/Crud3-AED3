4ec72a4a5d30870d8f7f98a9f2958ff72a29f545
TiagoMoscoso <79027488+TiagoMoscoso@users.noreply.github.com>
2023-03-11 11:49:53 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    String arquivoCSV = "src/netflix_titles_2021.csv";
+    BufferedReader br = null;
+    String linha = "";
+    String csvDivisor = ";";
+    try {
+
+        br = new BufferedReader(new FileReader(arquivoCSV));
+        while ((linha = br.readLine()) != null) {
+
+            String archive = linha;
+
+            System.out.println(archive);
+        }
+
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        if (br != null) {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+  }
+
+ }
